d02a2cef8b3a163e16bb9e868190dcc6dd7d44d9
leignel <cristian.leignel@gmail.com>
2023-11-03 14:59:15 -0300

    Css da home

--- projetinho-individual/src/pages/Home/index.jsx
+++ projetinho-individual/src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import Intro from "../../components/Intro";
 import Categorias from "../../components/Categorias";
+import "./style.css";
 
 const Home = () => {
   return (
     <div>
       <Intro />
+      <hr className="linha" />
       <Categorias />
     </div>
   );
